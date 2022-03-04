1b4ca9b8a205ffbb41efa27a0915b7221baf1987
Lydia Wallace <lydia.wallace@outlook.com>
2022-03-04 15:06:05 +0000

    adding growUp and walk methods

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,36 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
+  const pet = new Pet('Fido')
+  const pet2 = new Pet('hungryPet')
+  const pet3 = new Pet('unfitPet')
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+      expect(pet).toBeInstanceOf(Object);
     });
+    it('sets the name property', () => {
+      expect(pet.name).toEqual('Fido');
+    })
+    it('ages', () => {
+      expect(pet.age).toEqual(0);
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    })
+    it('gets hungry', () => {
+      expect(pet2.hunger).toEqual(0)
+      pet2.growUp();
+      expect(pet2.hunger).toEqual(5)
+    });
+    it('gets less fit', () => {
+      expect(pet3.fitness).toEqual(10)
+      pet3.growUp();
+      expect(pet3.fitness).toEqual(7)
+    });
+    it('gets fitter when it walks to a maximum of 10', () => {
+      pet3.walk();
+      expect(pet3.fitness).toEqual(10);
+      pet3.fitness=4
+      pet3.walk();
+      expect(pet3.fitness).toEqual(8);
+    }
+    )
   });
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,20 @@
-function Pet() {}
+function Pet(name) {
+    this.name = name
+    this.age = 0
+    this.hunger = 0
+    this.fitness = 10
+  }
+  Pet.prototype.growUp=function () {
+      this.age ++;
+      this.fitness-=3;
+      this.hunger +=5;
+      }
 
+  Pet.prototype.walk=function () {
+      const maxFitness= 10
+      if(this.fitness>=maxFitness-4) {
+        this.fitness=maxFitness;
+      } else {
+          this.fitness+=4}
+  }
 module.exports = Pet;
